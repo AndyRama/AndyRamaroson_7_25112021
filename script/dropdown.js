8da52c4ca817ef299904c0e144175d4f6c5d9240
AndyRama <andyramaroson@gmail.com>
2022-05-08 14:46:51 +0200

    [enhancement] dropdown grow container

--- script/dropdown.js
+++ script/dropdown.js
@@ -8,7 +8,18 @@ document.querySelectorAll(".filters__dropDown").forEach((btn) =>
 
 // function Open dropdownBtn on clic
 function openTaglist(idContainer) {
-  let tagContainer = document.getElementById("idContainer");
+  let tagContainer = document.getElementById(idContainer);
   const filtersForm = tagContainer.previousElementSibling;
   const icoDropDown = filtersForm.querySelector(".ico");
+
+  if (tagContainer.classList.contains("is-expanded")) {
+    tagContainer.classList.remove("is-expanded");
+    icoDropDown.classList.replace("ico__dropUp", "ico__dropDown");
+  } else {
+    if (document.querySelector(".filters__inputContainer.is-expanded") != null) {
+       document.querySelector(".filters__inputContainer.is-expanded").classList.remove("is-expanded");
+    }
+    tagContainer.classList.add("is-expanded");
+    icoDropDown.classList.replace("ico__dropDown", "ico__dropUp");
+  }
 }
