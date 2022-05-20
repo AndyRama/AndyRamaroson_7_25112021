aeaddf7376b1824b35a71083d4dfc07f95eec217
AndyRama <andyramaroson@gmail.com>
2022-05-20 16:46:45 +0200

    [bug] Comportement of dropdown filters closed not work

--- script/dropdown.js
+++ script/dropdown.js
@@ -2,21 +2,23 @@ const inputIng = document.getElementById("inputIng");
 const inputDev = document.getElementById("inputDev");
 const inputUst = document.getElementById("inputUst");
 
+const btnOrder = Array.from(document.querySelectorAll(".filters__dropDown"));
+
 // get all btn dropdown  
-document.querySelectorAll(".filters__dropDown").forEach((btn) =>
+btnOrder.forEach((btn,index) =>
   btn.addEventListener("click",(e) => {
     e.preventDefault();
     openTaglist(btn.getAttribute("aria-controls"));
-    
-    if (btn.getAttribute("aria-controls") == "ingredientsContainer") {
+
+    if(index == 0) {
       inputIng.classList.add("btn-Expansed");
     }
 
-    if (btn.getAttribute("aria-controls") == "deviceContainer") {
+    if(index == 1){
       inputDev.classList.add("btn-Expansed");
     }
 
-    if (btn.getAttribute("aria-controls") == "ustensilsContainer") {
+    if(index == 2) {
       inputUst.classList.add("btn-Expansed");
     }
   })
@@ -28,19 +30,18 @@ function openTaglist(idContainer) {
   const filtersForm = tagContainer.previousElementSibling;
   const icoDropDown = filtersForm.querySelector(".ico");
 
+
   if (tagContainer.classList.contains("is-expanded")) {
     tagContainer.classList.remove("is-expanded");
     icoDropDown.classList.replace("ico__dropUp", "ico__dropDown");
-    inputIng.classList.remove("btn-Expansed");
-    inputDev.classList.remove("btn-Expansed");
-    inputUst.classList.remove("btn-Expansed");
+    // inputIng.classList.remove("btn-Expansed");
+    // inputDev.classList.remove("btn-Expansed");
+    // inputUst.classList.remove("btn-Expansed");
 
   } else {
     if (document.querySelector(".filters__inputContainer.is-expanded") != null) {
       document.querySelector(".filters__inputContainer.is-expanded").classList.remove("is-expanded");
-      inputIng.classList.remove("btn-Expansed");
-      inputDev.classList.remove("btn-Expansed");
-      inputUst.classList.remove("btn-Expansed");
+
     }
     tagContainer.classList.add("is-expanded");
     icoDropDown.classList.replace("ico__dropDown", "ico__dropUp");   
