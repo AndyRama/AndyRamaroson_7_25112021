23720e7a3e0ad85341a94ab2568e619a4f42fd2c
AndyRama <andyramaroson@gmail.com>
2022-05-22 23:44:21 +0200

    [enhancement] add responsive for mobile

--- script/index.js
+++ script/index.js
@@ -118,7 +118,8 @@ function addFilter(e) {
   const type = e.dataset.type;
   const title = e.dataset.title;
   var htmlClass;
-
+  // const id = e.dataset.id;
+  
   if (type == "ingredients") {
     htmlClass = 'filters__btn--ingredients';
   }
@@ -132,7 +133,7 @@ function addFilter(e) {
   }
 
   document.getElementById('tagsBtn').innerHTML = document.getElementById('tagsBtn').innerHTML + `
-  <button onclick="removeFilter(this)" data-type="${type}" data-controls="${title}" class="filters__tag filters__Btn ${htmlClass}">
+  <button onclick="removeFilter(this);addCookie(this)" data-type="${type}" data-controls="${title}" class="filters__tag filters__Btn ${htmlClass}">
     ${title}
     <svg id="close" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M14.59 8L12 10.59L9.41 8L8 9.41L10.59 12L8 14.59L9.41 16L12 13.41L14.59 16L16 14.59L13.41 12L16 9.41L14.59 8ZM12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white"></path>
@@ -146,13 +147,20 @@ function addFilter(e) {
 function removeFilter(e) {
   e.remove();
   launchSearch();
-
 }
+
+// function addCookie(e) {
+//   const id = e.dataset.id;
+//   let cookie = localStorage.getItem(id);
+
+//   if(cookie == null) {
+//     localStorage.setItem(id,"click");
+//   }
+// }
 
 function searchKeyword() {
   launchSearch();
 }
-
 
 function launchSearch() {
   // Récupérer mes tags
@@ -169,21 +177,21 @@ function launchSearch() {
   const recipesArrayFiltered = [];
 
   recipesArray.map(recipe => {
-    const ustString = recipe.ustensils.join(", ")    
-    const ingString = recipe.ingredients
-      .map((ing) => ing.ingredient)
-      .join(", ");
+    // const ustString = recipe.ustensils.join(", ");  
+    // const ingString = recipe.ingredients
+    //   .map((ing) => ing.ingredient)
+    //   .join(", ");
 
     let haveTagOk = true;
     if (tagsStringList.length > 0) {
       tagsStringList.map(item => {
         if (item.type == 'ustensils') {  
-         
+      
         }
 
-        // if (item.type == 'ingredients') {
+        if (item.type == 'ingredients') {
 
-        // } 
+        } 
         //OK
         if (item.type == 'device') {
           if (recipe.appliance != item.title) {
