68bdae5b6138459f62d71483dad85eaa9255ac59
AndyRama <andyramaroson@gmail.com>
2022-06-02 23:11:00 +0200

    [fix] clean code add pdf

--- script/index.js
+++ script/index.js
@@ -185,15 +185,15 @@ function launchSearch() {
     let haveTagOk = true;
 
    if (tagsStringList.length > 0) {
-      tagsStringList.forEach(item => {
+      tagsStringList.map(item => {
       
       if(item.type == "ustensils")
-        if(!recipe.ustensils.reduce(ustensil => ustensil.toLowerCase() == item.title.toLowerCase())) {
+        if(!recipe.ustensils.some(ustensil => ustensil.toLowerCase() == item.title.toLowerCase())) {
           haveTagOk = false;
         }
 
       if(item.type == "ingredients")
-        if(!recipe.ingredients.reduce(ingredient => ingredient.ingredient.toLowerCase() == item.title.toLowerCase())) {
+        if(!recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() == item.title.toLowerCase())) {
           haveTagOk=false;
         }
 
@@ -211,7 +211,7 @@ function launchSearch() {
       const descriptionLowerCase = recipe.description.toLowerCase();
  
       let ingredientsSentence = '';
-      recipe.ingredients.reduce(ingredient => {
+      recipe.ingredients.map(ingredient => {
         ingredientsSentence = ingredientsSentence + ' ' + ingredient.ingredient;
       })
 
@@ -232,6 +232,8 @@ function launchSearch() {
   showErrorMessage(count);
 }
 
+
+// A REFACTORISER
 // bandeau informatif en cas d'absence de recette lors de la recette
 const searchBarInput = document.getElementById("search");
 const noRecipesMessage = document.getElementById("filtersMessage");
@@ -252,7 +254,7 @@ function showErrorMessage(count) {
   } else {
     noRecipesMessage.innerHTML = `
     <p class="filters__message--succes">
-       "Nous avons trouvés ${count} recettes"
+      "Nous avons trouvés ${count} recettes. "
       <svg id="closeM" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M14.59 8L12 10.59L9.41 8L8 9.41L10.59 12L8 14.59L9.41 16L12 13.41L14.59 16L16 14.59L13.41 12L16 9.41L14.59 8ZM12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white"></path>
       </svg>
@@ -267,12 +269,3 @@ function removeErrorMessage() {
   searchBarValue = "";
   searchBarInput.value = "";
 }
-
-//ALGO 2 => reduce
-//Branch create "checkout algo2"
-
-//button switch algo 1 , algo 2 
-
-//COMMENT TESTER LE CODE jsBRENCH
-
-
