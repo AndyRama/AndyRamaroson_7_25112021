7b8bfe5fc98e28fc65921ed5f59a8420af0be0f4
AndyRama <andyramaroson@gmail.com>
2022-06-03 23:56:03 +0200

    [Enhancement] tags filter update after keyword input >= 2

--- script/index.js
+++ script/index.js
@@ -252,15 +252,7 @@ function showErrorMessage(count) {
     noRecipesMessage.innerHTML = templateMessage;
     document.getElementById("closeM").addEventListener("click", removeErrorMessage);
   } else {
-    noRecipesMessage.innerHTML = `
-    <p class="filters__message--succes">
-      "Nous avons trouvés ${count} recettes. "
-      <svg id="closeM" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14.59 8L12 10.59L9.41 8L8 9.41L10.59 12L8 14.59L9.41 16L12 13.41L14.59 16L16 14.59L13.41 12L16 9.41L14.59 8ZM12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="white"></path>
-      </svg>
-    </p>        
-  `; 
-    document.getElementById("closeM").addEventListener("click", removeErrorMessage);
+    noRecipesMessage.innerHTML = ""; 
   }  
 }
 
@@ -269,3 +261,42 @@ function removeErrorMessage() {
   searchBarValue = "";
   searchBarInput.value = "";
 }
+
+const filtersInput = document.querySelectorAll(".filters__input");
+filtersInput.forEach((input) => {
+  input.addEventListener("keyup", (event) => {
+    if (event.target.value.length > 0) {
+      event.target.parentElement.nextElementSibling.classList.add(
+        "is-expanded"
+      );
+    } else {
+      event.target.parentElement.nextElementSibling.classList.remove(
+        "is-expanded"
+      );
+    }
+    switch (event.target.dataset.search) {
+      case "ingredients":
+        showTags(
+          allIngredients.filter((ing) => ing.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1),
+          "ingredientsTaglist",
+          "ingredients"
+        );
+        break;
+      case "devices":
+        showTags(
+          allDevices.filter((device) => device.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1),
+          "devicesTaglist",
+          "device"
+        );
+        break;
+      case "ustensils":
+        showTags(allUstensils.filter((ustensil) => ustensil.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1),
+          "ustensilsTaglist",
+          "ustensils"
+        );
+        break;
+      default:
+        break;
+    }
+  });
+});
